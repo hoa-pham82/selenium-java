ff3217a8f5049e9a9a3b689c36ff9c42e4bbee93
Hoa Pham <phamtthanhhoa@gmail.com>
2023-08-07 00:13:09 +0700

    test issue with CFT version

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,18 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.manager.SeleniumManager;
+
+import java.time.Duration;
+
 public class Test {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-
-        }
-    }
-}
+        ChromeOptions options = new ChromeOptions();
+//        options.setBrowserVersion("117");
+        String driverPath = SeleniumManager.getInstance().getDriverPath(options, false ).driverPath;
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://github.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        System.out.println(driverPath);
+        driver.quit();
+}}
