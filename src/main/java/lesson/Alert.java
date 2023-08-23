d4caffa5975b4d14aaf6086229d7e9238468d0e5
Hoa Pham <phamtthanhhoa@gmail.com>
2023-08-24 00:20:09 +0700

    add more test for alert handling

--- src/main/java/lesson/Alert.java
+++ src/main/java/lesson/Alert.java
@@ -2,6 +2,7 @@ package lesson;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,11 +15,31 @@ public class Alert {
   }
 
   @Test
-  void handleAlert() {
+  void handleAlertOK() {
     WebDriver driver = WebDriverManager.getDriver();
     driver.findElement(By.xpath("//button[@class='btn btn-default']")).click();
     driver.switchTo().alert().accept();
 
+  }
+
+  @Test
+  void handleAlertCancel() {
+    WebDriver driver = WebDriverManager.getDriver();
+    driver.findElement(By.xpath("//button[@class='btn btn-default btn-lg']")).click();
+    driver.switchTo().alert().dismiss();
+
+    Assert.assertEquals(driver.findElement(By.id("confirm-demo")).getText(), "You pressed Cancel!");
+  }
+
+  @Test
+  void handleAlertInput() {
+    WebDriver driver = WebDriverManager.getDriver();
+    driver.findElement(By.xpath("//button[normalize-space()='Click for Prompt Box']")).click();
+    driver.switchTo().alert().sendKeys("I agree!!");
+    driver.switchTo().alert().accept();
+
+    Assert.assertEquals(driver.findElement(By.id("prompt-demo")).getText(),
+        "You have entered 'I agree!!' !");
   }
 
   @AfterClass
