3a2a5608aaed85f9820808b88746faacefe6ecbb
Hoa Pham <phamtthanhhoa@gmail.com>
2023-08-25 03:14:00 +0700

    rename

--- src/main/java/lesson/PopUpWindow.java
+++ src/main/java/lesson/PopUpWindow.java
@@ -11,7 +11,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import support.WebDriverManager;
 
-public class popUpWindow {
+public class PopUpWindow {
 
   @BeforeClass
   void setUp() {
