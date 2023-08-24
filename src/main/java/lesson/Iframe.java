43bed6129fae28125ae420697cc809a0e5de1eda
Hoa Pham <phamtthanhhoa@gmail.com>
2023-08-25 02:21:46 +0700

    add test to handle iframe

---
+++ src/main/java/lesson/Iframe.java
@@ -0,0 +1,37 @@
+package lesson;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Iframe {
+
+  @BeforeClass
+  void setUp() {
+    WebDriverManager.setup("https://the-internet.herokuapp.com/iframe");
+  }
+
+  @Test
+  void handleIframe() {
+    WebDriver driver = WebDriverManager.getDriver();
+
+    // Find the iframe by its ID
+    WebElement iframe = driver.findElement(By.id("mce_0_ifr"));
+    driver.switchTo().frame(iframe);
+
+    // Interact with the text box
+    WebElement textbox = driver.findElement(By.cssSelector("#tinymce"));
+    textbox.clear();
+    textbox.sendKeys("Hi Christy!!");
+
+  }
+
+  @AfterClass(alwaysRun = false)
+  void tearDown() {
+    WebDriverManager.teardown();
+  }
+
+}
